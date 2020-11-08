7251475e55f19cd18131829800720a9f47337b3b
William Xie <williamxie168@berkeley.edu>
2020-11-08 23:08:30 +0800

    Add BasicEnemyMovement

---
+++ Assets/Scripts/EdgeBehavior.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class EdgeBehavior : MonoBehaviour {
+    void OnCollisionEnter2D(Collision2D other) {
+		other.gameObject.GetComponents<PlayerController>()[0].edge += 1;
+		Debug.Log(other.gameObject.GetComponents<PlayerController>()[0].edge);
+		Destroy(gameObject);
+	}
+}
