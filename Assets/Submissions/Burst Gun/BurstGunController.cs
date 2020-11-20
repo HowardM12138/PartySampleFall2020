477d4ea518295cfddccbb79b177d990607b62ea5
Zenuriken <henryla106@gmail.com>
2020-11-20 00:09:05 -0800

    Submissions
    Here are my submissions for the Burst Gun, Laser, Zolygon, and Dead Player Movement. For some reason GitHub only lets me submit to this branch. I will look into this on my end. Each submission and their scripts/prefabs are in their respective folders, as well as a test scene for each one.

---
+++ Assets/Submissions/Burst Gun/BurstGunController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class BurstGunController : MonoBehaviour {
+
+	[Header("Settings")]
+	public int damage;
+	public float bulletSpeed;
+	public float fireInterval = .5f;
+	public string damageTag = "Enemy";
+
+	[Header("Configurations")]
+	public Transform fireOrigin;
+	public GameObject bulletPrototype;
+
+	[Header("Status (Do not modify these fields through Editor)")]
+	public float lastFireTime;
+  
+
+	public void Trigger() {
+		if (CanFire()) {
+			StartCoroutine("Fire");
+		}
+	}
+
+	public bool CanFire() {
+		var time = Time.time;
+		return lastFireTime + fireInterval <= time;
+	}
+
+	IEnumerator Fire() {
+		for (float i = 0f; i < 3f; i++){
+            lastFireTime = Time.time;
+            var bullet = Instantiate(bulletPrototype, fireOrigin ? fireOrigin.position : transform.position, transform.rotation);
+            var controller = bullet.GetComponent<BulletController>();
+            controller.damage = damage;
+            controller.damageTag = damageTag;
+            var velocity = bullet.transform.up * bulletSpeed;
+            controller.GetComponent<Rigidbody2D>().velocity = velocity;
+            yield return new WaitForSeconds(0.07f);
+        }
+	}
+}
---
+++ Assets/Submissions/Dead Player Movement/DeadPlayerMovement.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeadPlayerMovement : MonoBehaviour {
+
+	[Header("Settings")]
+	public float speed;
+	public float MaxHorizDist;
+	public float MaxVertDist;
+	public KeyCode upKey = KeyCode.W;
+	public KeyCode downKey = KeyCode.S;
+	public KeyCode leftKey = KeyCode.A;
+	public KeyCode rightKey = KeyCode.D;
+	
+	[Header("Status (Do not modify these fields through Editor)")]
+	public float moveX;
+	public float moveY;
+
+	private void Update() {
+		
+		var vertical = 0f;
+		var horizontal = 0f;
+
+		// Cardinal Movement	
+		if (Input.GetKey(upKey)) {
+			vertical = speed;
+		} else if (Input.GetKey(downKey)) {
+			vertical = -speed;
+		}
+		if (Input.GetKey(leftKey)) {
+			horizontal = -speed;
+		} else if (Input.GetKey(rightKey)) {
+			horizontal = speed;
+		}
+
+		// Diagonal Movement + Vector Component Assignment
+		if (vertical != 0 && horizontal != 0) {
+			moveX = horizontal / Mathf.Sqrt(2f);
+			moveY = vertical / Mathf.Sqrt(2f);
+		} else {
+			moveX = horizontal;
+			moveY = vertical;
+		}
+
+		// Updates Dead Player's Position
+		transform.position = transform.position + new Vector3(moveX, moveY, 0);
+	
+		// Teleports Dead Player Back to Origin
+		if (Math.Abs(transform.position.x) > MaxHorizDist || Math.Abs(transform.position.y) > MaxVertDist){
+			transform.position = new Vector3(0, 0, 0);
+		}
+	
+	}
+}
+
